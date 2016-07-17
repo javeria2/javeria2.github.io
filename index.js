dbbc2574f2af74a886ff5b7f3b20ceef8b133fb8
Sanchay Javeria <javeria2@illinois.edu>
2016-07-17 07:00:11 +0530

    browser support

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ var block1 = document.querySelector(".block1");
 block1.addEventListener("click", function(){
 	alert("JS connected!");
 });
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	block1.classList.remove("block1");
-}
