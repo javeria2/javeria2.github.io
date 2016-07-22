9d6d569a46e2fa6b3eabd16851e3c60bc3ce57f1
Sanchay Javeria <javeria2@illinois.edu>
2016-07-22 06:57:51 +0530

    1st page almost done, picture remains

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-// if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-//  	//code...
-// }
 	
 // Initialize fullPage
-
   $('#fullpage').fullpage({
      anchors: ['homepage', 'personal-details','technical-skills','personal-projects','experience','education','contact'],
      slidesNavigation: true,
@@ -11,15 +7,21 @@
      navigationTooltips: ['Home', 'Personal Details', 'Technical skills' , 'Personal projects' , 'Experience', 'Education' , 'Contact'],
      controlArrows: false,
      sliding: true,
+     responsiveWidth: 1100,
       afterLoad: function(anchorLink, index) {
         if (index == 1) {
             $('#fp-nav').css({"top":"-100%","transition":"all 1s"});
+        }
+        if (index == 2) {
+          console.log("hi!!");
+          $('.box2').fadeIn(600);
         }
        	else if(index == 4){
 			$('.fp-slidesNav').css({"transition":"all 0.3s","left":"50%"});
 		}
       },
       onLeave: function(index, nextIndex, direction){
+        $('.box2').fadeOut(500);
         if(index == 1) {
           $('#fp-nav').css({"top":"50%","transition":"all 1s"});
           $('.fp-slidesNav').css({"transition":"all 1s", "left":"-100%"});
@@ -29,12 +31,3 @@
     }
   });
 
-// var get_ = document.querySelectorAll("#fp-nav ul li a.active span", 
-// 	".fp-slidesNav ul li a.active span", 
-// 	"#fp-nav ul li:hover a.active span", 
-// 	".fp-slidesNav ul li:hover a.active span");
-// console.log(get_);
-// get_[0].onmouseover = function(){
-// 	document.body.backgroundColor = 'black !important';
-// 	console.log("hi");
-// }
