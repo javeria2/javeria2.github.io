8ae4f3ae527314516cfb55e7b3d9d565629b26b3
Sanchay Javeria <javeria2@illinois.edu>
2016-08-30 04:14:53 -0500

    remove flash msg on reload

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,31 @@
 jQuery(document).ready(function($) {
 	$('nav').css('opacity', 0);
 
+
+
+
+	/*Helper function to extract the URL between the last / and before ? 
+	  If url is www.example.com/file.php?f_id=55 this function will return file.php 
+	 pseudo code: edit to match your url settings  
+	*/ 
+	function refineUrl()
+	{
+	    //get full url
+	    var url = window.location.href;
+	    //get url after/  
+	    var value = url.substring(url.lastIndexOf('/') + 1);
+	    //get the part before ?
+	    value  = value.split("/")[0];   
+	    return value;     
+	}
+
+	var newUrl = refineUrl();//fetch new url
+
+	//here you pass whatever you want to appear in the url after the domain /
+	window.history.pushState("object or string", "Title", "/"+newUrl );
+
+
+
 	//making nav title clickable
 	$('#sanchayjaveria').click(function(){
 		$('html, body').animate({
