7185ae7cf9efecbdf0aedc767a175159821d7458
Sanchay Javeria <javeria2@illinois.edu>
2016-08-30 04:21:39 -0500

    hi

--- mailer.php
+++ mailer.php
@@ -31,7 +31,7 @@
 	$headers = 'From: <test@test.com>' . "\r\n" .
 'Reply-To: <test@test.com>';
 
-mail('<myEmail@gmail.com>', 'the subject', 'the message', $headers,
+mail($recipient, 'the subject', 'the message', $headers,
   '-fwebmaster@example.com');
 
 
