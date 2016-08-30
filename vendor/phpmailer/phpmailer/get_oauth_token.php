76964177f50bbd5eec781f9eb77aa2ead7ca2a91
Sanchay Javeria <javeria2@illinois.edu>
2016-08-30 14:33:38 -0500

    test

--- vendor/phpmailer/phpmailer/get_oauth_token.php
+++ vendor/phpmailer/phpmailer/get_oauth_token.php
@@ -25,12 +25,14 @@ use Psr\Http\Message\ResponseInterface;
 session_start();
 
 //If this automatic URL doesn't work, set it yourself manually
-$redirectUri = isset($_SERVER['HTTPS']) ? 'https://' : 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+// $redirectUri = isset($_SERVER['HTTPS']) ? 'https://' : 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 //$redirectUri = 'http://localhost/phpmailer/get_oauth_token.php';
-
+$redirectUri = 'http://sanchayjaveria.herokuapp.com/phpmailer/phpmailer/get_oauth_token.php';
+$clientId = '183270531144-cijgu3a9psadmcqi8h9npfsgbiqp2232.apps.googleusercontent.com';
+$clientSecret = 'pe17OF8TebSxLaNX2vASTkBy';
 //These details obtained are by setting up app in Google developer console.
-$clientId = 'RANDOMCHARS-----duv1n2.apps.googleusercontent.com';
-$clientSecret = 'RANDOMCHARS-----lGyjPcRtvP';
+// $clientId = 'RANDOMCHARS-----duv1n2.apps.googleusercontent.com';
+// $clientSecret = 'RANDOMCHARS-----lGyjPcRtvP';
 
 class Google extends AbstractProvider
 {
